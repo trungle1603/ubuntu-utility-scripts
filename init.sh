c78702208be684a111ae84930a4a9bba48bc5ecc
caphesuada <leminhtrung.sp@gmail.com>
2023-03-03 03:32:02 +0000

    change init

--- init.sh
+++ init.sh
@@ -5,18 +5,15 @@ echo "Initial server"
 echo "The default services are installed: git, nodejs, yarn, pm2"
 
 echo "Choose option services: "
-
 echo "mongodb"
-
 echo "redis"
 
 read -p "Enter service you choose, separated by spaces: " services
 
-sudo bash ./git/install.sh
+# Install default
 sudo bash ./nodejs/install.sh
-sudo bash ./pm2/install.sh
-
-
+'
+# Install optional
 for service in $services
 do
   sudo bash ./$service/install.sh
