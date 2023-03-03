3d9c92449e7129c10ca2b59f0057b53df6e70368
caphesuada <leminhtrung.sp@gmail.com>
2023-03-03 03:37:32 +0000

    add comment hostname

--- get-server-ip.sh
+++ get-server-ip.sh
@@ -4,4 +4,6 @@ network_interface=$(ip route get 1.1.1.1 | grep -oP '(?<=dev\s)\w+' | head -1)
 # Get server ip address
 server_ip=$(ip -4 addr show $network_interface | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 
+# or just use: hostname -I
+
 echo "${server_ip}"
