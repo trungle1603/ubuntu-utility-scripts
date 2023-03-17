765ecf7b25def65582dc62dc5720f64a7a69db5b
nickytrung123 <leminhtrung.sp@gmail.com>
2023-03-17 17:16:28 +0700

    remove pm2, typescript, nest/cli

--- init.sh
+++ init.sh
@@ -2,19 +2,11 @@
 
 echo "Initial server"
 
-echo "The default services are installed: nodejs, yarn, pm2"
+echo "The Services Install:" 
+echo "NodeJs, MongoDB, Redis"
 
-echo "Choose option services: "
-echo "mongodb"
-echo "redis"
-
-read -p "Enter service you choose, separated by spaces: " services
-
-# Install default
 sudo bash ./nodejs/install.sh
 
-# Install optional
-for service in $services
-do
-  sudo bash ./$service/install.sh
-done
+sudo bash ./mongodb/install.sh
+
+sudo bash ./redis/install.sh
--- nodejs/install.sh
+++ nodejs/install.sh
@@ -9,10 +9,5 @@ sudo apt-get install -y nodejs build-essential
 # Confirm that Node.js is installed
 node -v
 
-# Install yarn, typescript, nestjs
+# Enable yarn
 sudo corepack enable
-sudo npm install -g typescript @nestjs/cli pm2@latest
-
-pm2 startup systemd
-pm2 save
-pm2 status
