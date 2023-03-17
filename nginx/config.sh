f86aba2298aed4a1824202c320ebf9f1b1f013fc
nickytrung123 <leminhtrung.sp@gmail.com>
2023-03-17 18:01:04 +0700

    add default config nginx

---
+++ nginx/config.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+read -p "Enter a domain name: " domain_name
+
+sudo mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
+sudo tee /etc/nginx/sites-available/default > /dev/null <<EOF
+server {
+    listen 80;
+    server_name $domain_name;
+
+    location / {
+        proxy_pass http://localhost:3000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
+    }
+}
+EOF
+
+# Restart Nginx
+sudo systemctl restart nginx
