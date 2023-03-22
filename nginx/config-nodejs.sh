3cb7be1b2663d9ccd600e10ce0d49a0ade35aba9
nickytrung123 <leminhtrung.sp@gmail.com>
2023-03-22 15:29:41 +0700

    22-03-2023

--- nginx/config-nodejs.sh
+++ nginx/config-nodejs.sh
@@ -3,7 +3,6 @@
 # THIS CONFIG FOR NODEJS ONLY
 
 read -p "Enter a domain name: " DOMAIN_NAME
-read -p "Enter a app name: " APP_NAME
 read -p "Enter a app port:" APP_PORT
 
 sudo tee /etc/nginx/sites-available/$DOMAIN_NAME > /dev/null <<EOF
@@ -21,8 +20,8 @@ server {
     ssl_certificate /etc/letsencrypt/live/$DOMAIN_NAME/fullchain.pem;
     ssl_certificate_key /etc/letsencrypt/live/$DOMAIN_NAME/privkey.pem;
 
-    location / {
-        proxy_pass http://localhost:$APP_PORT;
+    location /api {
+        proxy_pass http://localhost:$APP_PORT/api;
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection 'upgrade';
